3ea133053f99752b6651b328f1c5bb5fc6be1f6f
EmanuelVL <33427332+EmanuelVL@users.noreply.github.com>
2021-01-16 20:15:57 -0700

    Menú
    Faltan funciones dentro de las opciones, pero el esqueleto está terminado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import networkx as nx
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 import os
 
@@ -32,6 +32,7 @@ for v in g:
 
 print("nodo: %s"%(g.numeroNodos()))
 print("aristas: %s"%(g.numeroAristas()))
+print("\n-------------------------- MENÚ --------------------------")
 
 #df= pd.DataFrame({"de": de, 'a': a})
 
@@ -43,7 +44,7 @@ print("aristas: %s"%(g.numeroAristas()))
 def pedirOpcion():
     correcto= False
     num= -1
-     while(not correcto):
+    while(not correcto):
         try:
             num= int(input("Introduce tu opción: "))
             correcto= True
@@ -51,33 +52,201 @@ def pedirOpcion():
             print("Error: Introduce un número valido")
     return num
 
-opcion= -1
-sub_opcion= -1
 
 def menu():
-    while not salir:
-        print("1. Agregar")
-        print("2. Eliminar")
-        print("3. Buscar")
-        print("4. Grado de un Nodo")
-        print("5. Número total")
-        print("6. Vaciar")
-        print("0. Salir")
+    opcion = -1
+    sub_opcion = -1
+
+    while (opcion != 0):
+        print("1) Agregar")
+        print("2) Eliminar")
+        print("3) Buscar")
+        print("4) Grado de un Nodo")
+        print("5) Número total")
+        print("6) Vaciar")
+        print("0) Salir")
 
         while opcion not in range(7):
             opcion= pedirOpcion()
-        
+            if (opcion not in range(7)):
+                print("Selecciona una opción válida")
+
         if opcion == 1:
             menu_agregar()
+            opcion = -1
+        elif opcion == 2:
+            menu_eliminar()
+            opcion = -1
+        elif opcion == 3:
+            menu_buscar()
+            opcion = -1
+        elif opcion == 4:
+            menu_grado()
+            opcion = -1
+        elif opcion == 5:
+            menu_total()
+            opcion = -1
+        elif opcion == 6:
+            menu_vaciar()
+            opcion = -1
+
+    return
 
 def menu_agregar():
-    os.system("clear")
-    #os.system("cls")
-    print("1. Agregar Nodo")
-    print("2. Agregar Arista")
-    while sub_opcion not in range(3):
-        sub_opcion= pedirOpcion()
-    
-    if sub_opcion == 1:
-        print("Intruduce la cla")
+    global sub_opcion
+    sub_opcion = -1
 
+    while (sub_opcion != 0):
+        #os.system("clear") # LINUX
+        os.system("cls") # WINDOWS
+
+        print("1) Agregar Nodo")
+        print("2) Agregar Arista")
+        print("0) Regresar")
+
+        while sub_opcion not in range(3):
+            sub_opcion= pedirOpcion()
+            if (sub_opcion not in range(3)):
+                print("Selecciona una opción válida")
+
+        if sub_opcion == 1:
+            # Función agregar Nodo (Pedir id)
+            print("Agregar nodo")
+            sub_opcion = -1
+        elif sub_opcion == 2:
+            # Función agregar Nodo (Pedir id)
+            print("Agregar arista")
+            sub_opcion = -1
+
+    return
+
+def menu_eliminar():
+    global sub_opcion
+    sub_opcion = -1
+
+    while (sub_opcion != 0):
+        #os.system("clear") # LINUX
+        os.system("cls") # WINDOWS
+
+        print("1) Eliminar Nodo")
+        print("2) Eliminar Arista")
+        print("0) Regresar")
+
+        while sub_opcion not in range(3):
+            sub_opcion= pedirOpcion()
+            if (sub_opcion not in range(3)):
+                print("Selecciona una opción válida")
+
+        if sub_opcion == 1:
+            # Función eliminar Nodo (Pedir id)
+            print("Eliminar nodo")
+            sub_opcion = -1
+        elif sub_opcion == 2:
+            # Función eliminar Nodo (Pedir id)
+            print("Eliminar arista")
+            sub_opcion = -1
+
+    return
+
+def menu_buscar():
+    global sub_opcion
+    sub_opcion = -1
+
+    while (sub_opcion != 0):
+        #os.system("clear") # LINUX
+        os.system("cls") # WINDOWS
+
+        print("1) Buscar Nodo")
+        print("2) Buscar Arista")
+        print("0) Regresar")
+
+        while sub_opcion not in range(3):
+            sub_opcion= pedirOpcion()
+            if (sub_opcion not in range(3)):
+                print("Selecciona una opción válida")
+
+        if sub_opcion == 1:
+            # Función buscar Nodo (Pedir id)
+            print("Buscar nodo")
+            sub_opcion = -1
+        elif sub_opcion == 2:
+            # Función buscar Nodo (Pedir id)
+            print("Buscar arista")
+            sub_opcion = -1
+
+    return
+
+def menu_grado():
+    #os.system("clear") # LINUX
+    os.system("cls") # WINDOWS
+
+    # Función obtener grado de un Nodo
+    print("Grado de un nodo")
+
+    return
+
+def menu_total():
+    global sub_opcion
+    sub_opcion = -1
+
+    while (sub_opcion != 0):
+        #os.system("clear") # LINUX
+        os.system("cls") # WINDOWS
+
+        print("1) Total de Nodos")
+        print("2) Total de Aristas")
+        print("0) Regresar")
+
+        while sub_opcion not in range(3):
+            sub_opcion= pedirOpcion()
+            if (sub_opcion not in range(3)):
+                print("Selecciona una opción válida")
+
+        if sub_opcion == 1:
+            # Función total Nodo (Pedir id)
+            print("Total de nodos")
+            sub_opcion = -1
+        elif sub_opcion == 2:
+            # Función total Nodo (Pedir id)
+            print("Total de aristas")
+            sub_opcion = -1
+
+    return
+
+def menu_vaciar():
+    global sub_opcion
+    sub_opcion = -1
+    warning_op = ''
+
+    while (sub_opcion != 0):
+        #os.system("clear") # LINUX
+        os.system("cls") # WINDOWS
+
+        print("1) Vaciar Nodo")
+        print("2) Vaciar Grafica")
+        print("0) Regresar")
+
+        while sub_opcion not in range(3):
+            sub_opcion= pedirOpcion()
+            if (sub_opcion not in range(3)):
+                print("Selecciona una opción válida")
+
+        if sub_opcion == 1:
+            # Función vaciar Nodo (Pedir id)
+            print("Vaciar nodo")
+            sub_opcion = -1
+        elif sub_opcion == 2:
+            print("Seguro que quieres vaciar la grafica? (Empezar desde cero)")
+            while(warning_op != 's' and warning_op != 'S' and warning_op != 'n' and warning_op != 'N'):
+                warning_op = input("s/N: ")
+
+            if (warning_op == 'S' or warning_op == 's'):
+                # Función vaciar gráfica
+                print("Vaciar gráfica")
+                return
+            elif (warning_op == 'N' or warning_op == 'n'):
+                sub_opcion = -1
+
+    return
+
+menu()
